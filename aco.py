ab7bea4cd8b886b22ba2276c9f8d6cd5dc3e7a73
Bartosz Bartosik <babosz189@gmail.com>
2023-11-11 17:34:30 +0100

    Created objective function; created aco class

---
+++ aco.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class AntColonyOptimization:
+
+    def __init__(self, objective_function, ants_number, evaporation_rate, Q):
+        self.f = objective_function
+        self.m = ants_number
+        self.ro = evaporation_rate
+        self.Q = Q
+        self.tau = np.array([])
+        self.dtau = np.array([])
+
+
+    def update_dtau(self):
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import numpy as np
+
+import aco as ACO
+
 def main():
-    pass
+
+    def objective_function(cities, path):
+        sum = 0
+        for i in range(len(path))[1:]:
+            sum += np.sqrt((cities[path[i]][0] - cities[path[i-1]][0])**2 + (cities[path[i]][1] - cities[path[i-1]][1])**2)
+        sum += np.sqrt((cities[path[-1]][0] - cities[path[0]][0])**2 + (cities[path[-1]][1] - cities[path[0]][1])**2)
+        return sum
+
+
+    cities = np.array([
+        [0, 0],     # 0
+        [4, 1],     # 1
+        [6, 4],     # 2
+        [8, 2],     # 3
+        [2, 4],     # 4
+    ])
+
+
+    sample_path = np.array([0, 2, 4, 3, 1])
+
+    print(objective_function(cities, sample_path))
 
 
 if __name__ == '__main__':
