9ec3390a943736fb7a12447dabf012cf741dc2ea
Bartosz Bartosik <babosz189@gmail.com>
2023-11-22 18:25:26 +0100

    Added documentation to ACO class; Encapsulated ACO variables

--- aco.py
+++ aco.py
@@ -9,19 +9,19 @@ class AntColonyOptimization:
 
     def __init__(self, graph: tuple, ants_number: int, evaporation_rate: float, alpha, beta, Q):
         # Unpack graph values
-        self.C, self.L = graph
+        self.__C, self.__L = graph
         # # #
-        self.m = ants_number
-        self.ro = evaporation_rate
-        self.alpha = alpha
-        self.beta = beta
-        self.Q = Q
+        self.__m = ants_number
+        self.__ro = evaporation_rate
+        self.__alpha = alpha
+        self.__beta = beta
+        self.__Q = Q
         # # #
         # Inner variables
-        self.ants = []
-        self.eta = 1/np.array(self.L)
-        self.tau = np.zeros_like(self.L)
-        self.tau_init()
+        self.__ants = []
+        self.__eta = 1 / np.array(self.__L)
+        self.__tau = np.zeros_like(self.__L)
+        self.__tau_init()
 
         self.solutions = {
             's': [],
@@ -30,71 +30,84 @@ class AntColonyOptimization:
         }
 
 
-    def run(self, iterations):
-        solution, value = 0, 0
+    def run(self, iterations) -> tuple:
+        """
+        Run algorithm specified number of times.
+        Returns permutation of nodes corresponding to the optimal solution.
+        """
         for i in range(iterations):
             # Show progress bar
             sys.stdout.write('\r')
             sys.stdout.write("Ants path construction in progress: [%-20s] %d%%" % ('=' * int(i / iterations * 20), int(i / iterations * 100)))
             sys.stdout.flush()
             # Spawn new ants
-            self.ants = [Ant() for _ in range(self.m)]
+            self.__ants = [Ant() for _ in range(self.__m)]
 
             # Construct tour for each ant
-            solution, value = self.tour_construction()
+            solution, value = self.__tour_construction()
             # Append the one which did best to the solutions
             self.solutions['s'].append(solution)
             self.solutions['values'].append(value)
 
             # Update pheromone trails
-            self.update_pheromones()
+            self.__update_pheromones()
 
         min_value = min(self.solutions['values'])
         best_solution = self.solutions['s'][self.solutions['values'].index(min_value)]
         return best_solution, min_value
 
 
-    def update_pheromones(self):
-        self.tau *= (1-self.ro)
+    def __update_pheromones(self):
+        """
+        Update and save tau matrix using pheromone evaporation parameter 'ro', and solution constructed by ants using parameter 'Q' and length of the tour they traveled.
+        """
+        self.__tau *= (1 - self.__ro)
 
-        for ant in self.ants:
+        for ant in self.__ants:
             path = ant.M
             for i in range(len(ant.M)):
-                self.tau[path[i - 1], path[i]] += self.Q/ant.L
-        self.solutions['tau'].append(self.tau.copy())
+                self.__tau[path[i - 1], path[i]] += self.__Q / ant.L
+        self.solutions['tau'].append(self.__tau.copy())
 
 
-    def tour_construction(self):
+    def __tour_construction(self) -> tuple:
+        """
+        Create ant solutions. Each ant starts at randomly chosen node and selects the next one parallely with other ants.
+        After ants finish their tours, the one which path was the shortest is selected in order to return its permutation of visited nodes, and the length of the tour it traveled.
+        """
         min_value = sys.maxsize
         solution = 0
 
-        nodes = len(self.C)
+        nodes = len(self.__C)
         for node in range(nodes-1):
-            for ant in self.ants:
+            for ant in self.__ants:
                 if len(ant.M) == 0:
                     ant.M.append(random.choice(range(nodes)))
-                j = self.select_next_node(ant.M)
+                j = self.__select_next_node(ant.M)
                 ant.M.append(j)
-                ant.L += self.L[ant.M[-2], j]
+                ant.L += self.__L[ant.M[-2], j]
                 if node == nodes - 2:
-                    ant.L += self.L[ant.M[0], ant.M[-1]]
+                    ant.L += self.__L[ant.M[0], ant.M[-1]]
                     if ant.L < min_value:
                         min_value = ant.L
                         solution = ant.M
         return solution, min_value
 
 
-    def select_next_node(self, M):
+    def __select_next_node(self, M):
+        """
+        For the given list of nodes, choose the next one and return its index.
+        """
         # Current node
         i = M[-1]
 
         feasible_nodes = []
         p = np.array([])
-        for j in range(len(self.L[i])):
+        for j in range(len(self.__L[i])):
             # Consider j which are not present in the M list
             if j not in M[:-1]:
                 feasible_nodes.append(j)
-                p_ij = self.tau[i][j]**self.alpha * self.eta[i][j]**self.beta
+                p_ij = self.__tau[i][j] ** self.__alpha * self.__eta[i][j] ** self.__beta
                 p = np.append(p, p_ij)
 
         p = p/sum(p)
@@ -103,20 +116,24 @@ class AntColonyOptimization:
         return j
 
 
-    def tau_init(self):
+    def __tau_init(self):
+        """
+        Initialize tau matrix using nearest neighbour heuristic.
+        """
         _, L = self.nearest_neighbour()
-        self.tau += self.m/L
+        self.__tau += self.__m / L
 
 
-    def nearest_neighbour(self):
+    def nearest_neighbour(self) -> tuple:
         """
         Solve graph problem by starting at random node and keep choosing the nearest one until any is left.
+        Returns tuple consisting of nodes permutation and its value for the given problem.
         """
         # Initialize solution list with randomly chosen node
-        s = [np.random.randint(len(self.C))]
+        s = [np.random.randint(len(self.__C))]
 
         # Iterate until s list is not full
-        while len(s) < len(self.C):
+        while len(s) < len(self.__C):
             # Current node
             i = s[-1]
 
@@ -125,11 +142,11 @@ class AntColonyOptimization:
             nearest_value = sys.maxsize
 
             # Consider all j nodes linked to i
-            for j in range(len(self.L[i])):
+            for j in range(len(self.__L[i])):
                 # Consider j which are not present in the s list
                 if j not in s[:-1]:
-                    if self.L[i][j] < nearest_value:
-                        nearest_value = self.L[i][j]
+                    if self.__L[i][j] < nearest_value:
+                        nearest_value = self.__L[i][j]
                         nearest_index = j
 
             # Get nearest neighbour j
@@ -137,6 +154,6 @@ class AntColonyOptimization:
 
         total_value = 0
         for i in range(len(s)):
-            total_value += self.L[s[i-1], s[i]]
+            total_value += self.__L[s[i - 1], s[i]]
 
         return s, total_value
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
     ])
 
     # Randomly generate cities coordinates
-    cities_number = 50
+    cities_number = 20
     cities = np.array(np.unique(np.random.random(size=(cities_number, 2)), axis=1)*10)
     print(cities)
 
@@ -26,7 +26,7 @@ def main():
     tsp = TSP(cities)
 
     # Initialize Ant System algorithm
-    m = 30
+    m = 10
     ro = 0.5
     alpha = 1
     beta = 2
@@ -39,7 +39,9 @@ def main():
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # # # # # # # # # # # # # # # # # # # # #   V I S U A L I S A T I O N   # # # # # # # # # # # # # # # # # # # # # #
-    # PREPROCESS OBTAINED DATA
+
+    ################################
+    ### PREPROCESS OBTAINED DATA ###
     # Optimal solution
     xs = [cities[n][0] for n in s]
     ys = [cities[n][1] for n in s]
@@ -48,12 +50,10 @@ def main():
     ys.append(ys[0])
 
     # Paths history
-    solutions = aco.solutions['s']
-
     xh = []
     yh = []
 
-    for h in solutions:
+    for h in aco.solutions['s']:
         x = [cities[n][0] for n in h]
         y = [cities[n][1] for n in h]
         x.append(x[0])
@@ -77,10 +77,14 @@ def main():
 
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    #########################
+    ### INSTANTIATE PLOTS ###
     # FILENAME
     filename = 'cities-{}_m-{}_ro-{}_a-{}_b-{}_Q-{}'.format(cities_number, m, ro, alpha, beta, Q)
 
     # CREATE PLOTS
+    # Solutions history
     plt.subplot(212)
     plt.plot(aco.solutions['values'], c='0.3')
     plt.title('shortest distance obtained')
@@ -88,6 +92,7 @@ def main():
     plt.ylabel('value')
     plt.grid()
 
+    # Cities
     plt.subplot(221)
     plt.scatter(cities[:, 0], cities[:, 1], c='0.25')
     plt.title('cities')
@@ -96,6 +101,7 @@ def main():
     plt.gca().set_aspect('equal')
     plt.grid()
 
+    # Cities with optimal path
     plt.subplot(222)
     plt.scatter(cities[:, 0], cities[:, 1], c='0.25')
     plt.plot(xs, ys, c='0.3')
@@ -105,18 +111,24 @@ def main():
     plt.gca().set_aspect('equal')
     plt.grid()
 
+    # SAVE PLOT
     plt.gcf().set_size_inches(8, 10)
     plt.savefig("plots/solution-{}.png".format(filename), dpi=200)
     plt.show()
 
-
+    # ANIMATED PLOT WITH SOLUTION EVOLUTION
+    # Plot cities
     fig, ax = plt.subplots()
     ax.scatter(cities[:, 0], cities[:, 1], c='0.25')
     ax_city_links = np.zeros_like(alphas[0], dtype=object)
+
+    # Plot pheromone links between cities
     for i in range(len(cities)):
         for j in range(len(cities)):
             if i != j:
                 ax_city_links[i][j] = ax.plot([cities[i, 0], cities[j, 0]], [cities[i, 1], cities[j, 1]], c='0.5', alpha=0.3, zorder=0)[0]
+
+    # Plot optimal path
     ax_links = ax.plot(xs[0], ys[0], c='0.3', alpha=1)[0]
     iteration_template = 'iteration = %.0f'
     iteration_text = ax.text(0, -0.11, '', transform=ax.transAxes)
@@ -124,30 +136,38 @@ def main():
     ax.set_ylabel('y')
     ax.set_aspect('equal')
     ax.grid()
+
+    # Animation function
     def update(iteration):
-        # For each iteration update the x and y coordinates of links
+        # For each iteration update the optimal path
         x = xh[iteration]
         y = yh[iteration]
 
+        # For each iteration update pheromone trails visibility
         for i in range(len(alphas[0])):
             for j in range(len(alphas[0])):
                 if ax_city_links[i][j] != 0:
                     ax_city_links[i][j].set_alpha(alphas[iteration][i][j])
 
-        # Update the line plot
+        # Update the optimal path
         ax_links.set_xdata(x)
         ax_links.set_ydata(y)
 
-        # Update text
+        # Update text with iteration number
         iteration_text.set_text(iteration_template % (iteration+1))
+
         return ax_links, ax_city_links
 
+    # Time in milliseconds per frame
     interval = 100
+
+    # Show the plot
     anim = animation.FuncAnimation(fig=fig, func=update, frames=len(xh), interval=interval)
     plt.show()
 
+    # Save animation
     writergif = animation.PillowWriter(fps=len(xh)/interval*10)
-    anim.save("plots/tour_construction-{}.gif".format(filename), writer=writergif, dpi=200)
+    anim.save("plots/tour_construction-{}.gif".format(filename), writer=writergif, dpi=150)
 
 if __name__ == '__main__':
     main()
